b1e33e56db48608a063448197ed079e6b2118b75
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-11-17 14:56:46 +0300

    Add files via upload

---
+++ sehirler/src/tourData.js
@@ -0,0 +1,37 @@
+export const tourData = [
+  {
+    id : 1,
+    city : "İstanbul",
+    img : "./img/istanbul.png",
+    name : "İstanbul Tarihi Turlar",
+    info : "İstanbul, Türkiye'de Marmara Bölgesi'nin Çatalca-Kocaeli Bölümü'nde yer alan şehir ve Türkiye'nin 81 ilinden biridir. Ülkenin nüfus bakımından en çok göç alan ve en kalabalık ilidir. Ekonomik, tarihî ve sosyo-kültürel açıdan önde gelen şehirlerden biridir."
+  },
+  {
+    id : 2,
+    city : "Ankara",
+    img : "./img/ankara.png",
+    name : "Ankara Şehir Turu",
+    info : "Ankara, Türkiye'nin başkenti ve en kalabalık ikinci ilidir. Nüfusu 2022 yılı itibarıyla 5.747.325 kişidir. Bu nüfus; 25 ilçe ve bu ilçelere bağlı 1425 mahallede yaşamaktadır. İl genelinde nüfus yoğunluğu 215'tir."
+  },
+  {
+    id : 3,
+    city : "İzmir",
+    img : "./img/izmir.png",
+    name : "İzmir Şehir Turu",
+    info : "İzmir, Türkiye'de Ege Bölgesi'nde yer alan şehir ve ülkenin 81 ilinden biridir. Ülkenin nüfus bakımından en kalabalık üçüncü şehridir. Ekonomik, tarihi ve sosyo-kültürel açıdan önde gelen şehirlerden biridir. Nüfusu 2021 itibarıyla 4.425.789 kişidir. Yüzölçümü olarak ülkenin yirmi üçüncü büyük ilidir."
+  },
+  {
+    id : 4,
+    city : "Bursa",
+    img : "./img/bursa.png",
+    name : "Bursa Şehir Turu",
+    info : "Bursa, Türkiye'nin bir ili ve en kalabalık dördüncü şehri. 2021 itibarıyla 3.147.818 nüfusa sahiptir. Marmara Bölgesinin Güney Marmara bölümünde, 40° batı boylam ve 29° kuzey enlem daireleri arasında yer alır. Eski adı Hüdavendigâr'dır."
+  },
+  {
+    id : 5,
+    city : "Antalya",
+    img : "./img/antalya.png",
+    name : "Antalya Şehir Turu",
+    info : "Antalya, Türkiye'nin bir ili ve en kalabalık beşinci şehridir. 2021 sonu itibarıyla il nüfusu 2.619.832'dir. Türkiye'de turizmin başkenti olarak görülmektedir. Özellikle yaz aylarında turist sayısı artar. Şehrin yüzölçümü 20.177 km²dir. İlde km²ye 130 kişi düşmektedir."
+  }
+  ]
